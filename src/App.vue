9333a9fce9877bb5e2d6d774dd2a95cf858e91ca
AndreyBS1 <andrey.babenkov.00@gmail.com>
2022-03-04 01:08:54 +0300

    Restyling

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div id="app">
     <intro></intro>
-    <header>
-      <b-navbar class='navbar'>
-        <div class='project-name'>L A N D I N G</div>
-      </b-navbar>
-    </header>
     <main>
       <section class="info-section">
         <div class='text-section'>
@@ -42,37 +37,25 @@ export default {
 
 <style>
 #app {
-  font-family: cursive;
+  font-family: "Rany", sans-serif;
   margin: 0;
   padding: 0;
-  color: snow;
-  text-shadow:  black 1px 1px 1px;
-}
-
-header {
-  font-size: 110%;
-  padding: 0.7% 2%;
-  background-color: #e27d60;
+  color: #1b1d1c;
 }
 
 main {
   display: flex;
   flex-direction: row;
   justify-content: center;
-  background-color: #41b3a3;
+  background-color: whitesmoke;
 }
 
 .info-section {
-  width: 70vw;
+  width: 100vw;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 5% 3%;
-  margin: 5% 0;
-  background-color: #e8a87c;
-  border: 20px solid #c38d9e;
-  border-radius: 70px 20px;
-  box-shadow: 10px 10px 3px #85dcb8;
+  padding: 10% 6%;
 }
 
 .text-section {
--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,39 +1,10 @@
 <template>
-    <div v-show="intro" class="intro">
-        <div
-            class="welcome-text"
-            v-show="firstWelcomeText"
-            @click="clickOnFirstWelcomeText()"
-        >
-            <h1>Добро пожаловать!</h1>
-            <h4>Тыкните куда-нибудь</h4>
-        </div>
-        <div
-            class="welcome-text"
-            v-show="secondWelcomeText"
-            @click="clickOnSecondWelcomeText()"
-        >
-            <h1>
-                Решил вот сделать себе лендинг =D
-            </h1>
-        </div>
-        <div
-            class="welcome-text"
-            v-show="thirdWelcomeText"
-            @click="clickOnThirdWelcomeText()"
-        >
-            <h1>
-                Думаю, это будет нагляднее, чем слова в резюме.
-            </h1>
-        </div>
-        <div
-            class="welcome-text"
-            v-show="fourthWelcomeText"
-            @click="clickOnFourthWelcomeText()"
-        >
-            <h1>
-                Ссылки на мои проекты Вы найдете в конце страницы.
-            </h1>
+    <div class="intro">
+        <div class="background-prism">
+            <div class="welcome-text">
+                <h1>LANDING</h1>
+                <h6>или резюме Бабенкова Андрея</h6>
+            </div>
         </div>
     </div>
 </template>
@@ -41,50 +12,30 @@
 <script>
 export default {
     name: 'Intro',
-    data() {
-        return {
-            intro: true,
-            firstWelcomeText: true,
-            secondWelcomeText: false,
-            thirdWelcomeText: false,
-            fourthWelcomeText: false,
-        }
-    },
-    methods: {
-        clickOnFirstWelcomeText() {
-            this.firstWelcomeText = false;
-            this.secondWelcomeText = true;
-        },
-        clickOnSecondWelcomeText() {
-            this.secondWelcomeText = false;
-            this.thirdWelcomeText = true;
-        },
-        clickOnThirdWelcomeText() {
-            this.thirdWelcomeText = false;
-            this.fourthWelcomeText = true;
-        },
-        clickOnFourthWelcomeText() {
-            this.fourthWelcomeText = false;
-            this.intro = false;
-        }
-    }
 }
 </script>
 
 <style scoped>
 .intro {
     height: 100vh;
-    background-color: #85c8b8;
+    background: no-repeat center / cover url(../assets/background_1.jpg);
+}
+
+.background-prism {
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(to right, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.8));
 }
 
 .welcome-text {
     width: 100%;
     height: 100%;
-    padding: 3%;
+    padding: 10%;
     display: flex;
     flex-direction: column;
     justify-content: center;
-    align-items: center;
-    font-size: 5%;
+    align-items: flex-end;
+    /* font-size: 150%; */
+    color: whitesmoke;
 }
 </style>
