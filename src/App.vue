4eefec98fb8d8e32a3fd74ffa9df2e1fe4a37723
AndreyBS1 <andrey.babenkov.00@gmail.com>
2022-03-03 21:16:20 +0300

    Created Intro

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <intro></intro>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Intro from './components/Intro.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Intro,
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: cursive;
 }
 </style>
---
+++ src/components/Intro.vue
@@ -0,0 +1,91 @@
+<template>
+    <div v-show="intro" class="intro">
+        <div
+            class="welcome-text"
+            v-show="firstWelcomeText"
+            @click="clickOnFirstWelcomeText()"
+        >
+            <h1>Добро пожаловать!</h1>
+            <h6>Тыкните куда-нибудь</h6>
+        </div>
+        <div
+            class="welcome-text"
+            v-show="secondWelcomeText"
+            @click="clickOnSecondWelcomeText()"
+        >
+            <h1>
+                Решил вот сделать себе лендинг =D
+            </h1>
+        </div>
+        <div
+            class="welcome-text"
+            v-show="thirdWelcomeText"
+            @click="clickOnThirdWelcomeText()"
+        >
+            <h1>
+                Думаю, это будет нагляднее, чем слова в резюме.
+            </h1>
+        </div>
+        <div
+            class="welcome-text"
+            v-show="fourthWelcomeText"
+            @click="clickOnFourthWelcomeText()"
+        >
+            <h1>
+                Ссылки на мои проекты Вы найдете в конце страницы.
+            </h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Intro',
+    data() {
+        return {
+            intro: true,
+            firstWelcomeText: true,
+            secondWelcomeText: false,
+            thirdWelcomeText: false,
+            fourthWelcomeText: false,
+        }
+    },
+    methods: {
+        clickOnFirstWelcomeText() {
+            this.firstWelcomeText = false;
+            this.secondWelcomeText = true;
+        },
+        clickOnSecondWelcomeText() {
+            this.secondWelcomeText = false;
+            this.thirdWelcomeText = true;
+        },
+        clickOnThirdWelcomeText() {
+            this.thirdWelcomeText = false;
+            this.fourthWelcomeText = true;
+        },
+        clickOnFourthWelcomeText() {
+            this.fourthWelcomeText = false;
+            this.intro = false;
+        }
+    }
+}
+</script>
+
+<style scoped>
+.intro {
+    width: 100vw;
+    height: 100vh;
+    background-color: indigo;
+}
+.welcome-text {
+    width: 100%;
+    height: 100%;
+    padding: 3%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 5%;
+    color: whitesmoke;
+}
+</style>
